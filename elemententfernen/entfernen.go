488a4acebe9ca81d379bf5060391dd9a76239b0b
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-08 16:42:38 +0100

    Mehr gelöste Aufgaben hinzugefügt.

--- elemententfernen/entfernen.go
+++ elemententfernen/entfernen.go
@@ -3,6 +3,15 @@ package elemententfernen
 // Liefert eine Liste, die alle Elemente aus list enthält,
 // außer dem an Stelle pos.
 func RemoveElement(list []int, pos int) []int {
-	// TODO
-	return nil
+	if len(list) == 0 || pos < 0 {
+		return []int{}
+	}
+
+	head := list[:1]
+	tail := list[1:]
+	if pos == 0 {
+		return tail
+	}
+
+	return append(head, RemoveElement(tail, pos-1)...)
 }
--- suche/binsuche.go
+++ suche/binsuche.go
@@ -1,6 +1,17 @@
 package suche
 
 func BinSearch(list []int, key int) int {
-	// TODO
-	return 0
+
+	if len(list) == 0 {
+		return 0
+	}
+	pos_mitte := len(list) / 2
+
+	if key == list[pos_mitte] {
+		return pos_mitte
+	}
+	if key < list[pos_mitte] {
+		return BinSearch(list[:pos_mitte], key)
+	}
+	return BinSearch(list[pos_mitte+1:], key) + pos_mitte + 1
 }
