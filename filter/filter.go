1a74ae3a9b8317a8ae6d7c37d552f2421581a5d4
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-08 13:06:11 +0100

    Filter-Aufgabe hinzugefügt.

---
+++ filter/filter.go
@@ -0,0 +1,13 @@
+package filter
+
+// Liefert eine Liste mit allen Elementen aus list, die kleiner oder gleich key sind.
+func FilterLess(list []int, key int) []int {
+	// TODO
+	return nil
+}
+
+// Liefert eine Liste mit allen Elementen aus list, die größer als key sind.
+func FilterGreater(list []int, key int) []int {
+	// TODO
+	return nil
+}
